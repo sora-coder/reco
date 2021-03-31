05cd6aca51cbb59464254752fd6e88da8cf996bd
sora-coder <rqqgx271@yahoo.co.jp>
2021-03-31 17:24:09 +0900

    掲示板トップページ追加

--- www/html/index.php
+++ www/html/index.php
@@ -1,2 +1,25 @@
 <?php
-phpinfo();
+    require('dbconnect.php');
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>top</title>
+</head>
+<body>
+    <div id="wrap">
+        <header>
+            <h1>reco</h1>
+        </header>
+        <div id="content">
+            <div><a href="logout">ログアウト</a></div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
